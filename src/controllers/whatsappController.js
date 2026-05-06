ef32cb27b7abfaa449dcacfb01c6dd49fb0e9e0d
juan pablo <juanpabloqb1990@gmail.com>
2026-05-06 15:43:01 -0500

    v5 path deleted

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -1,8 +1,5 @@
-
-import { logToFile, readLogs } from '../utils/logger.js';
-
 export const VerifiToken = (req, res) => {
-    //res.send('Token verified successfully!');
+
     try {
         const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
         const token = req.query['hub.verify_token'];
@@ -17,48 +14,19 @@ export const VerifiToken = (req, res) => {
     }
 }
 
-
 export const ReceiveMessage = (req, res) => {
     try {
-        const body = req.body;
-
-        // Validación segura (anti-crash)
-        const value = body?.entry?.[0]?.changes?.[0]?.value;
-
-        if (!value) {
-            logToFile({ error: "Invalid body", body });
-            return res.send("no data");
-        }
-
-        // Puede venir messages o statuses
-        if (value.messages) {
-            logToFile({ type: "messages", data: value.messages });
-        } else {
-            logToFile({ type: "other_event", data: value });
-        }
-
-        res.status(200).send("event received");
-
+       
+        const entry = req.body['entry'][0];
+        const changes = entry['changes'][0];
+        const value = changes['value'];
+        const messages = value['messages'];
+        console.log(JSON.stringify(messages, null, 2));
+        
+        res.send("event received");
     } catch (error) {
-        logToFile({ error: error.message });
-        res.status(500).send("event doesnt received");
+        console.log(error);
+        
+        res.send("event doesnt received");
     }
-};
-
-
-// Endpoint para ver logs
-export const GetLogs = (req, res) => {
-    try {
-        const logs = readLogs();
-
-        if (!logs) {
-            return res.send("No logs");
-        }
-
-        res.setHeader('Content-Type', 'text/plain');
-        res.send(logs);
-
-    } catch (error) {
-        res.status(500).send("Error reading logs");
-    }
-};
+}
--- src/routes/whatsappRoutes.js
+++ src/routes/whatsappRoutes.js
@@ -3,11 +3,9 @@ import fs from 'fs';
 
 const route = express.Router();
 
-import { VerifiToken, ReceiveMessage, GetLogs } from '../controllers/whatsappController.js';
+import { VerifiToken, ReceiveMessage } from '../controllers/whatsappController.js';
 
-route.get('/verify', VerifiToken);
-route.post('/message', ReceiveMessage);
-// Ver logs
-route.get('/logs', GetLogs);
+route.get('/', VerifiToken);
+route.post('/', ReceiveMessage);
 
 export default route;

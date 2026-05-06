aee8a8a867f6ca98a1e3dfb716036480837e59b8
juan pablo <juanpabloqb1990@gmail.com>
2026-05-06 11:35:51 -0500

    verify implemented v2 edited body by query

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -3,7 +3,7 @@ export const VerifiToken = (req, res) => {
     try {
         const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
         const token = req.query['hub.verify_token'];
-        const challenge = req.body['hub.challenge'];
+        const challenge = req.query['hub.challenge'];
         if(token === VERIFY_TOKEN && challenge != null && token != null) {
             res.status(200).send(challenge);
         }else{

8505a0d3b1f66fb51503914277a5037826f07a53
juan pablo <juanpabloqb1990@gmail.com>
2026-05-06 11:18:14 -0500

    verify implemented v1

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -1,5 +1,17 @@
 export const VerifiToken = (req, res) => {
-    res.send('Token verified successfully!');
+    //res.send('Token verified successfully!');
+    try {
+        const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+        const token = req.query['hub.verify_token'];
+        const challenge = req.body['hub.challenge'];
+        if(token === VERIFY_TOKEN && challenge != null && token != null) {
+            res.status(200).send(challenge);
+        }else{
+            res.status(403).json({ error: 'Token verification failed.' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: 'An error occurred while verifying the token.' });
+    }
 }
 
 export const ReceiveMessage = (req, res) => {

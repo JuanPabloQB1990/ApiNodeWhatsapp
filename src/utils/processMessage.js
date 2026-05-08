37fe398ae43cd830a9e28661de42f61abd9a97a1
juan pablo <juanpabloqb1990@gmail.com>
2026-05-08 13:18:24 -0500

    processMessage implemented v4

--- src/utils/processMessage.js
+++ src/utils/processMessage.js
@@ -1,5 +1,5 @@
 import { sendMessageWhatsapp } from "../services/whatsappService.js";
-import { messageText } from "./whatsappModels.js";
+import { messageComprar, messageList, messageText } from "./whatsappModels.js";
 
 export const processMessage = (textUser, numberPhone) => {
     textUser = textUser.toLowerCase();
@@ -8,9 +8,20 @@ export const processMessage = (textUser, numberPhone) => {
     if (textUser.includes("hola") || textUser.includes("buenos dias") || textUser.includes("buenas tardes") || textUser.includes("buenas noches")) {
         let model = messageText("¡Hola! ¿En qué puedo ayudarte?", numberPhone);
         models.push(model);
+        let modelList = messageList(numberPhone);
+        models.push(modelList);
     }else if (textUser.includes("gracias")) {
         let model = messageText("¡De nada! ¿En qué más puedo ayudarte?", numberPhone);
         models.push(model);
+        
+    }else if (textUser.includes("comprar")) {
+        let model = messageComprar(numberPhone);
+        models.push(model);
+        
+    }else if (textUser.includes("vender")) {
+        let model = messageText("Registrate para vender tus productos", numberPhone);
+        models.push(model);
+        
     }else if (textUser.includes("adios") || textUser.includes("adiós")  || textUser.includes("hasta luego") || textUser.includes("nos vemos")) {
         let model = messageText("¡Hasta luego! Gracias por escribirme", numberPhone);
         models.push(model);
--- src/utils/whatsappModels.js
+++ src/utils/whatsappModels.js
@@ -1,12 +1,110 @@
 export function messageText(textResponse, numberPhone) {
   const data = JSON.stringify({
     messaging_product: "whatsapp",
-    preview_url: false,
+    preview_url: true,
     recipient_type: "individual",
     to: numberPhone,
     type: "text",
     text: {
       body: textResponse,
+    },
+  });
+
+  return data;
+}
+
+export function messageList(numberPhone) {
+  const data = JSON.stringify({
+        "messaging_product": "whatsapp",
+        "to": numberPhone,
+        "type": "interactive",
+        "interactive": {
+            "type": "list",
+            "body": {
+                "text": "✅ Tengo estas opciones"
+            },
+            "footer": {
+                "text": "Selecciona una de las opciones para poder atenderte"
+            },
+            "action": {
+                "button": "Ver opciones",
+                "sections": [
+                    {
+                        "title": "Compra y vende productos",
+                        "rows": [
+                            {
+                                "id": "main-comprar",
+                                "title": "Comprar",
+                                "description": "Compra los mejores productos para tu hogar"
+                            },
+                            {
+                                "id": "main-vender",
+                                "title": "Vender",
+                                "description": "Vende tus productos"
+                            }
+                        ]
+                    },
+                    {
+                        "title": "📍Centro de atención",
+                        "rows": [
+                            {
+                                "id": "main-agencia",
+                                "title": "Agencia",
+                                "description": "Puedes visitar nuestra agencia."
+                            },
+                            {
+                                "id": "main-contacto",
+                                "title": "Centro de contacto",
+                                "description": "Te atenderá uno de nuestro agentes."
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    });
+
+  return data;
+}
+
+export function messageComprar(numberPhone) {
+  const data = JSON.stringify({
+    messaging_product: "whatsapp",
+    to: numberPhone,
+    type: "interactive",
+    interactive: {
+      type: "button",
+      body: {
+        text: "¿Que producto quieres comprar?",
+      },
+      footer: {
+        text: "Tenemos los siguientes productos disponibles",
+      },
+      action: {
+        buttons: [
+          {
+            type: "reply",
+            reply: {
+              id: "portatil-01",
+              title: "Portatil Lenovo",
+            },
+          },
+          {
+            type: "reply",
+            reply: {
+              id: "portatil-02",
+              title: "Portatil Dell",
+            },
+          },
+          {
+            type: "reply",
+            reply: {
+              id: "computador-desktop-03",
+              title: "Computador Desktop",
+            },
+          }
+        ],
+      },
     },
   });
 
